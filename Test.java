c31a2ba59bfeb3fbf4d080c16248a756dc8e82ce
parvmunjal <parvmunjal100@gmail.com>
2025-04-01 00:28:11 +0530

    Initial changes

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Myself parv");
+        System.out.println(new Date());
+        System.out.println("Jenkins practice");
+    }
+}
